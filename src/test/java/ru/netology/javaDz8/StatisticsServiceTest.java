b5b832460e54723b470e30dce379da6078a0974b
Yury Kozachek <show_@mail.ru>
2022-12-24 14:27:46 +0300

    commit add test

---
+++ src/test/java/ru/netology/javaDz8/StatisticsServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.javaDz8;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatisticsServiceTest {
+@Test
+    void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+    Assertions.assertEquals(expected,actual);
+    }
+    @Test
+    void findMaxNew() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 166, 11, 11, 12};
+        long expected = 166;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected,actual);
+    }
+}
